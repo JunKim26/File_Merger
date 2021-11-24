2282db617fe742429a95e3f52574e8f19484a47f
JunKim26 <54968347+JunKim26@users.noreply.github.com>
2021-11-24 10:42:49 -0800

    Update csv_merge.py

--- csv_merge.py
+++ csv_merge.py
@@ -32,16 +32,62 @@ def main():
 
     def csv_opener():
         """ this function is used for the button to open the csv file """
-
-        global csv_folder
-        csv_folder = askdirectory()                                                         # show an "Open" dialog box and return the path to the selected folder
-
-        end_button = Button(window, text = 'Create', command =window.destroy).pack()        # button to close tkinter window
-                                                                    
-    csv_button = Button(window, text = 'Open Folder', command = csv_opener).pack()
-
-    window.mainloop()                                                                       # tells Python to run the Tkinter event loop
-
+        
 # =======================================================================================================================================================
 #                                                           Main Function Section
 # =======================================================================================================================================================
+
+    def rename_column(rename_df):
+        
+        rename_df.rename(columns={
+                        'cs_hid' : 'Household ID', 
+                        'profile_url': 'Linkedin Link', 
+                        'email':"Email",
+                        'full_name':"Household Name",
+                        'first_name':"First Name",
+                        'last_name':"Last Name",
+                        'avatar':"LinkedIn Profile Picture URL",
+                        'location_name':"Location",
+                        'industry':"Industry",
+                        'address':"Address",
+                        'birthday':"Birthdate",
+                        'organization_1':"Employed By",
+                        'organization_title_1':"Title",
+                        'organization_start_1':"Hire Date",
+##                      'organization_2':"",
+##                      'organization_title_2':"",
+##                      'organization_3':"",
+##                      'organization_title_3':"",
+                        'education_1':"Education",
+##                      'education_degree_1':"",
+##                      'education_fos_1':"",
+                        'education_start_1':"Education Start Date",
+                        'education_end_1':"Education End Date",
+##                      'education_2':"",
+##                      'education_degree_2':"",
+##                      'education_fos_2':"",
+##                      'education_start_2':"",
+##                      'education_end_2':"",
+##                      'education_3':"",
+##                      'education_degree_3':"",
+##                      'education_fos_3':"",
+##                      'education_start_3':"",
+##                      'education_end_3':"",
+##                      'language_1':"Language",
+##                      'language_proficiency_1':"",
+##                      'language_2':"",
+##                      'language_proficiency_2':"",
+##                      'language_3':"",
+##                      'language_proficiency_3':"",
+                        'languages':"Languages",
+                        'phone_1':"Phone",
+                        'phone_type_1':"Phone Type",
+##                      'phone_2':"",
+##                      'phone_type_2':"",
+##                      'mutual_first_fullname':"",
+##                      'mutual_second_fullname':""
+                        'address': 'location',
+                        'City': 'Billing City', 
+                        'State': 'Billing State/Province'
+                            }, inplace=True
+                        )
