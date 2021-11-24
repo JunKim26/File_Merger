6e1d6f2888caa75adde32eab6719b3b8bfdad2cb
JunKim26 <54968347+JunKim26@users.noreply.github.com>
2021-11-24 10:40:56 -0800

    Update csv_merge.py

--- csv_merge.py
+++ csv_merge.py
@@ -21,3 +21,27 @@ def main():
 # =======================================================================================================================================================
 #                                                           Script lines for Tkinter GUI
 # =======================================================================================================================================================
+
+
+
+    window = tk.Tk()                                                                        # creates a tkinter object
+    window.geometry('200x200')                                                              # set size of tkinter window
+
+    label = tk.Label(text='CSV Merger')                                                     # sets the text to be dipslayed by tkinter
+    label.pack()
+
+    def csv_opener():
+        """ this function is used for the button to open the csv file """
+
+        global csv_folder
+        csv_folder = askdirectory()                                                         # show an "Open" dialog box and return the path to the selected folder
+
+        end_button = Button(window, text = 'Create', command =window.destroy).pack()        # button to close tkinter window
+                                                                    
+    csv_button = Button(window, text = 'Open Folder', command = csv_opener).pack()
+
+    window.mainloop()                                                                       # tells Python to run the Tkinter event loop
+
+# =======================================================================================================================================================
+#                                                           Main Function Section
+# =======================================================================================================================================================
