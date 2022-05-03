3d806a2979b3846d9a561745beb96ac98f7fa889
JunKim26 <54968347+JunKim26@users.noreply.github.com>
2022-05-03 11:14:41 -0700

    Update CSV_merger.py

--- CSV_merger.py
+++ CSV_merger.py
@@ -8,63 +8,57 @@ import os
 import glob
 import pandas as pd
 import numpy as np
-import tkinter as tk                                                                        # used as a user friendly tool for the program
+import tkinter as tk                                                                        
 from tkinter import *
-from tkinter.filedialog import askdirectory                                                 # used to open csv file
+from tkinter.filedialog import askdirectory                                                 
 from datetime import datetime      
 
 
 def main():
 
-# =======================================================================================================================================================
-#                                                           Script lines for Tkinter GUI
-# =======================================================================================================================================================
+    # initialize Tkinter GUI
+    window = tk.Tk()                                                                        
+    window.geometry('200x200')                                                              
 
-
-
-    window = tk.Tk()                                                                        # creates a tkinter object
-    window.geometry('200x200')                                                              # set size of tkinter window
-
-    label = tk.Label(text='CSV Merger')                                                     # sets the text to be dipslayed by tkinter
+    label = tk.Label(text='CSV Merger')                                                    
     label.pack()
 
     def directory_opener():
         """ this function is used for the button to open the csv file """
 
         global csv_folder
-        csv_folder = askdirectory()                                                         # show an "Open" dialog box and return the path to the selected folder
+        csv_folder = askdirectory()                                                         
 
-        end_button = Button(window, text = 'Create', command =window.destroy).pack()        # button to close tkinter window
+        end_button = Button(window, text = 'Create', command =window.destroy).pack()        
                                                                     
     csv_button = Button(window, text = 'Open Folder', command = directory_opener).pack()
 
-    window.mainloop()                                                                       # tells Python to run the Tkinter event loop
-
-# =======================================================================================================================================================
-#                                                           Main Function Section
-# =======================================================================================================================================================
+    window.mainloop()                                                                       
 
 
     def csv_merge():
+        """ merges csv files in a folder """
+        
         os.chdir(csv_folder)
         extension = 'csv'
         all_filenames = [i for i in glob.glob('*.{}'.format(extension))]
-                                                                                    
-        combined_csv = pd.concat([pd.read_csv(f) for f in all_filenames ])                  # combine all files in the list
+        
+        # combine all files in the list         
+        combined_csv = pd.concat([pd.read_csv(f) for f in all_filenames ])                  
         csv_df = pd.DataFrame(combined_csv)    
-
-        dt = datetime.now().strftime('%Y.%m.%d-%I%M%S%p')                                   # year_month_day-hours_minutes_seconds_AM/PM ; used in Title                
-        dt_string = str(dt)                                                                 # string of date and time
+        
+        # year_month_day-hours_minutes_seconds_AM/PM ; used in Title   
+        dt = datetime.now().strftime('%Y.%m.%d-%I%M%S%p')                                              
+        dt_string = str(dt)                                                               
         file_name = dt_string +" combined.csv"
 
-        script_dir = os.path.dirname(__file__)                                              # absolute directory the script is in
+        script_dir = os.path.dirname(__file__)                                              
         rel_path = 'Output'
-        abs_file_path = os.path.join(script_dir, rel_path)                                  # this joins the absolute path of current script with wanted relative path
-
-        csv_df = csv_df.dropna(axis=1, how='all')                                           # this drops all columns without any values
-
-
-        # =====================================================================
+        
+        # this joins the absolute path of current script with wanted relative path
+        abs_file_path = os.path.join(script_dir, rel_path)                
+        
+        csv_df = csv_df.dropna(axis=1, how='all')                                          
 
         last_path = os.path.basename(os.path.normpath(csv_folder))    
 
